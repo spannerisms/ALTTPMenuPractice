f30c86c2d85ea4b0da8c49ad13565f22f6b0cc15
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-11-23 13:03:11 -0500

    what the hell

--- Practice/PlayerMovement.java
+++ Practice/PlayerMovement.java
@@ -33,7 +33,8 @@ public class PlayerMovement {
 		BufferedImage ret = new BufferedImage(BG_WIDTH, BG_HEIGHT, BufferedImage.TYPE_4BYTE_ABGR);
 		Graphics g = ret.getGraphics();
 		int size = moves.length;
-		for (int i = 0; i < size; i++) {			PlayerMovement p = moves[i];			ItemPoint cursorLoc = ItemPoint.valueOf("SLOT_" + p.LOCATION);
+		for (int i = 0; i < size; i++) {
+			PlayerMovement p = moves[i];			ItemPoint cursorLoc = ItemPoint.valueOf("SLOT_" + p.LOCATION);
 			g.drawImage(PLAYER_MOVES[p.MOVEMENT],
 					ITEM_ORIGIN_X + cursorLoc.x - CURSOR_OFFSET,
 					ITEM_ORIGIN_Y + cursorLoc.y - CURSOR_OFFSET,
